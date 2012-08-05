cb046b16f4a06251f7763c59bbab5eeab6664165
Ruoran Wang <ruoranwang@gmail.com>
2012-08-05 15:46:15 -0500

    basically working

--- lib/chef/knife/tree.rb
+++ lib/chef/knife/tree.rb
@@ -32,34 +32,89 @@ module RoleTreePlugin
       @loader ||= Chef::Knife::Core::ObjectLoader.new(Chef::Role, ui)
     end
 
-    def read_role(role_name) 
-      File.open(role_name, 'r+') do |f|
+    # Read roles of a parent role
+    def read_role(p_role_name)
+      role_list = []
+      start = false
+      p_role_name += ".rb" unless p_role_name =~ /.+\.rb/
+      file_full_path = File.join(File.expand_path(Chef::Config[:role_path]), p_role_name)
+      File.open(file_full_path, 'r+') do |f|
         lines = f.readlines
         lines.each do |line|
-          if line =~ /^version\s+["'](\d+)\.(\d+)\.(\d+)["'].*$/
-            major = $1
-            minor = $2
-            patch = $3
-            current_version = "#{major}.#{minor}.#{patch}"
-            available_versions = Chef::CookbookVersion.available_versions(cookbook_name)
-            if available_versions.nil?
-              Chef::Log.info("User added a new cookbook: #{cookbook_name}")
+          start = true if line.include? "run_list" 
+
+          if start
+            next if line =~ /.+#.+["']/
+            if line =~ /["']role\[(.+)\]["']/
+              role_list << $1 
+            elsif line =~ /["']recipe\[(.+)\]["']/
+              role_list << "*" + $1 
+            elsif line =~ /\"(.+)\"/
+              role_list << "*" + $1 
             end
           end
+
+          break if line.include? ")" and start == true  
+        end
+      end
+      role_list
+    end
+
+    # Read recipes of a role
+    def read_recipe(p_role_name) 
+      recipe_list = []
+      start = false
+      file_full_path = File.join(File.expand_path(Chef::Config[:role_path]), p_role_name)
+      File.open(file_full_path, 'r+') do |f|
+        lines = f.readlines
+        lines.each do |line|
+          start = true if line.include? "run_list" 
+
+          if start
+            next if line =~ /.+#.+["']/
+          end
+
+          break if line.include? ")" and start == true  
+        end
+      end
+      recipe_list
+    end
+
+    def tree(roles=[], n=1)
+      roles.each do |r|
+        if r.include? "*"
+          puts "  " * n + r
+          next
+        end
+        roles = read_role(r)
+        if roles.nil?
+          return
+        else
+          puts "  " * n + r
+          tree(roles, n+1)
         end
       end
     end
 
     def run
       self.config = Chef::Config.merge!(config)
+
+      if config[:role]
+        puts "read a single role"
+        tree([config[:role]])
+      else
+        puts "read all role"
+        role_path = File.expand_path(Chef::Config[:role_path])
+        loaded_roles = loader.find_all_objects(role_path)
+        tree(loaded_roles)
+      end
       
-      role_path = "/Users/ruoran/work/chef/roles"
-      Chef::Log.info(role_path)
-      puts role_path
+      role_path = File.expand_path(Chef::Config[:role_path])
 
       loaded_roles = loader.find_all_objects(role_path)
 
-      puts loaded_roles
+      loaded_roles.each do |role|
+      end
 
       puts 'end of tree'
     end #run end
